12def1e71acb27c13def36d9ed8af56d57374e1f
Yaouen Vandenbossche <yva@atolcd.com>
2018-08-30 12:15:42 +0200

    Modification du composant App-tag-component
    - Composant déplacé de la doc list à l'onglet commentaires
    
    - Aspect et comportement du composant modifiés afin de s'adapter et de
    correspondre aux besoins et à l'apparence de l'appli adf-notepad

--- src/app/tag/tag.component.ts
+++ src/app/tag/tag.component.ts
@@ -1,14 +1,72 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter , Output, OnInit, OnChanges } from '@angular/core';
+import { TagService } from '@alfresco/adf-content-services';
+import { TranslationService } from '@alfresco/adf-core';
+
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html'
 })
-export class TagComponent {
+export class TagComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(private tagService: TagService,
+              private translateService: TranslationService) {}
 
   @Input()
   nodeId;
 
+  @Output()
+  successAdd: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  error: EventEmitter<any> = new EventEmitter();
+
+  errorMsg: string;
+
+  newTagName: string;
+
+  tagsEntries: any;
+
+  ngOnInit() {
+    if (this.nodeId) {
+      this.tagService.getTagsByNodeId(this.nodeId).subscribe((data) => {
+          this.tagsEntries = data.list.entries;
+      });
+    }
+  }
+
+  ngOnChanges() {
+    this.cleanErrorMsg();
+    if (this.nodeId) {
+      this.tagService.getTagsByNodeId(this.nodeId).subscribe((data) => {
+          this.tagsEntries = data.list.entries;
+      });
+    }
+  }
+
+  cleanErrorMsg() {
+    this.errorMsg = '';
+  }
+
+  searchTag(searchTagName: string) {
+    if (this.tagsEntries) {
+        return this.tagsEntries.find((currentTag) => {
+            return (searchTagName === currentTag.entry.tag);
+        });
+    }
+  }
+
+  addTag() {
+    if (this.searchTag(this.newTagName)) {
+        this.translateService.get('TAG.MESSAGES.EXIST').subscribe((error) => {
+            this.errorMsg = error;
+        });
+        this.error.emit(this.errorMsg);
+    } else {
+        this.tagService.addTag(this.nodeId, this.newTagName).subscribe(() => {
+            this.newTagName = '';
+            this.successAdd.emit(this.nodeId);
+        });
+    }
+  }
 }
