41c63365447baec81d068a446f9482cacb5386b2
Yaouen Vandenbossche <yva@atolcd.com>
2018-09-21 15:11:48 +0200

    Ajout d'action sur le contenu de la corbeille
    - Possibilité de supprimer définitivement et ede restaurer les fichiers
    dans la corbeille
    
    - I18n correspondant
    
    - pop up
    
    - content action

--- src/app/trashcan/trashcan.component.ts
+++ src/app/trashcan/trashcan.component.ts
@@ -1,14 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ConfirmDialogComponent, DocumentListComponent} from '@alfresco/adf-content-services';
+import { TranslationService, NodesApiService, NotificationService } from '@alfresco/adf-core';
+
 
 @Component({
   selector: 'app-trashcan',
   templateUrl: './trashcan.component.html'
 })
-export class TrashcanComponent implements OnInit {
+export class TrashcanComponent {
 
-  constructor() { }
+  @ViewChild('documentList')
+  documentList: DocumentListComponent;
 
-  ngOnInit() {
+  constructor(private nodesApiService: NodesApiService,
+              private translationService: TranslationService,
+              private notificationService: NotificationService,
+              private dialog: MatDialog) {}
+
+  restore(event: any): Promise<any> {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+          title: this.translationService.instant('NOTIFICATION.TITLE'),
+          message: this.translationService.instant('NOTIFICATION.RESTORE_MESSAGE'),
+          yesLabel: this.translationService.instant('NOTIFICATION.YES'),
+          noLabel: this.translationService.instant('NOTIFICATION.NO')
+      },
+      minWidth: '250px'
+    });
+    return new Promise((resolve) => {
+      dialogRef.beforeClose().subscribe(result => {
+      if (result) {
+        this.nodesApiService.restoreNode(event.value.entry.id).subscribe(() => {
+           this.documentList.reload();
+        });
+        this.notificationService.openSnackMessage(this.translationService.instant('NOTIFICATION.FILE_RESTORED'));
+      } else {
+        this.notificationService.openSnackMessage(this.translationService.instant('NOTIFICATION.CANCEL'));
+      }
+      resolve();
+      });
+    });
   }
+
+  delete(event: any): Promise<any> {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+          title: this.translationService.instant('NOTIFICATION.TITLE'),
+          message: this.translationService.instant('NOTIFICATION.DELETE_MESSAGE'),
+          yesLabel: this.translationService.instant('NOTIFICATION.YES'),
+          noLabel: this.translationService.instant('NOTIFICATION.NO')
+      },
+      minWidth: '250px'
+    });
+    return new Promise((resolve) => {
+      dialogRef.beforeClose().subscribe(result => {
+      if (result) {
+        this.nodesApiService.restoreNode(event.value.entry.id).subscribe(() => {
+          this.nodesApiService.deleteNode(event.value.entry.id, {'permanent': true}).subscribe(() => {
+            this.documentList.reload();
+         });
+       });
+        this.notificationService.openSnackMessage(this.translationService.instant('NOTIFICATION.FILE_PERM_DELETED'));
+      } else {
+        this.notificationService.openSnackMessage(this.translationService.instant('NOTIFICATION.CANCEL'));
+      }
+      resolve();
+      });
+    });
+   }
 
 }
