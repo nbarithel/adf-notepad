fb31e0cc6dd4cb1d059e67c2b6d593e8896d8771
Yaouen Vandenbossche <yva@atolcd.com>
2018-10-03 11:52:20 +0200

    Amélioration directive appResizer: rightEl
    - Changement de fichier scss provoquant un disfonctionnement de la
    directive appResizer
    
    - Directive appResizer modifiée, la barre suit bien la souris et elle
    fonctionne que lorsqu'on a cliqué sur une note

--- src/app/resizer.directive.ts
+++ src/app/resizer.directive.ts
@@ -7,34 +7,37 @@ export class ResizerDirective {
 
   private el: HTMLElement;
 
-  oldW = 100;
-
   leftElement: HTMLElement;
 
-  constructor(el: ElementRef,
-              private renderer: Renderer) {
+  righElement: HTMLElement;
+
+  constructor(el: ElementRef, private renderer: Renderer) {
     this.el = el.nativeElement;
   }
 
   @HostListener('mouseover') onmouseover() {
     this.el.style.cursor = 'col-resize';
     this.leftElement = document.getElementById('leftEl');
-    const click = this.renderer.listen(this.el , 'mousedown', (event) => {
-      if (event) {
-        const move = this.renderer.listenGlobal('document', 'mousemove', (event2) => {
-          this.mouseMove(event2);
-        });
-        this.renderer.listenGlobal('document', 'mouseup', () => {
-          move();
-          click();
-        });
-      }
-    });
+    this.righElement = document.getElementById('rightEl');
+    if (this.righElement) {
+      const click = this.renderer.listen(this.el , 'mousedown', (event) => {
+        if (event) {
+          this.mouseMove(event);
+          const move = this.renderer.listenGlobal('document', 'mousemove', (event2) => {
+            this.mouseMove(event2);
+          });
+          const up = this.renderer.listenGlobal('document', 'mouseup', () => {
+            move();
+            click();
+            up();
+          });
+        }
+      });
+    }
   }
 
   mouseMove(event: MouseEvent) {
-    this.oldW += event.movementX / 10;
-    this.leftElement.style.width = this.oldW + '%';
+    this.leftElement.style.width = event.clientX - 200 + 'px';
   }
 
 }
