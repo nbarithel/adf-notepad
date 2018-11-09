36704a0b6e0777f2b423df314490e1b52d132c13
Yaouen Vandenbossche <yva@atolcd.com>
2018-11-09 10:35:45 +0100

    Ajout de tests méthodes composant comment
    - Ajout de tests sur les méthodes du composant.
    Test clear(), isATask(), isANode(), isReadOnly(),
    ngOnChanges()...

--- src/app/comment/comments.component.spec.ts
+++ src/app/comment/comments.component.spec.ts
@@ -11,10 +11,10 @@ fdescribe('CommentsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-       imports: [
+      imports: [
         AppTestingModule,
         TranslateModule,
-        ],
+      ],
       declarations: [
         CommentsComponent
       ],
@@ -32,4 +32,42 @@ fdescribe('CommentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('clear method test', () => {
+    component.message = 'message';
+    component.clear();
+    expect(component.message).toBe('');
+  });
+
+  it('isATask method test', () => {
+    component.taskId = 'task';
+    expect(component.isATask()).toBeTruthy();
+    component.taskId = '';
+    expect(component.isATask()).not.toBeTruthy();
+  });
+
+  it('isANode method test', () => {
+    component.nodeId = 'node';
+    expect(component.isANode()).toBeTruthy();
+    component.nodeId = '';
+    expect(component.isANode()).not.toBeTruthy();
+  });
+
+  it('isReadOnly method test', () => {
+    component.readOnly = true;
+    expect(component.isReadOnly()).toBeTruthy();
+    component.readOnly = false;
+    expect(component.isReadOnly()).toBeFalsy();
+  });
+
+  fit('ngOnChanges  method test', () => {
+    const comment = new CommentModel();
+    component.comments = [comment, comment];
+    component.ngOnChanges({taskId: '', nodeId: ''});
+    expect(component.comments).toEqual([]);
+  });
+
+  it('add method test', () => {
+  });
+
 });
